a9526d23cfae3efb2ba8ad1f15b701f88ae0c5f1
Mads Freek <freek@wayf.dk>
2022-10-24 15:27:30 +0200

    Updated the websp "server"

--- sshwebclient.php
+++ sshwebclient.php
@@ -1,5 +1,5 @@
 <?php
 header('content-type: text/plain');
-$fn = "/var/run/sshcerts/".$_GET['token'];
+$fn = "/var/run/sshweblogin/".$_GET['token'];
 print_r(json_decode(file_get_contents($fn), 1));
 unlink($fn);
