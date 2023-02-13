8cef942eb792be99cf198a595a0c1069302258d9
Mads Freek <freek@wayf.dk>
2023-02-13 23:05:50 +0100

    Removed SAML2jwt support - now depends on Oidc_claim_edupersonprincipalname from mod_auth_openidc. For now removed support for additional data from OP in certificate - only sub/principalname needed

--- go/sshca.go
+++ go/sshca.go
@@ -1,26 +1,16 @@
 package main
 
 import (
-	"crypto"
 	"crypto/rand"
-	"crypto/rsa"
-	"crypto/sha256"
-	"crypto/sha512"
-	"crypto/tls"
-	"crypto/x509"
 	"embed"
-	"encoding/base64"
 	"encoding/hex"
 	"encoding/json"
-	"errors"
 	"fmt"
 	"html/template"
 	"io"
-	"io/ioutil"
 	"log"
 	"net"
 	"net/http"
-	"net/url"
 	"regexp"
 	"strings"
 	"sync"
@@ -81,149 +71,31 @@ func ca() {
 
 func caHandler(w http.ResponseWriter, r *http.Request) {
 	r.ParseForm()
-	v := r.Form
-
-	host := r.Host
-
-	showForm := (host == "sshsp.lan" || v.Get("idplist") == "") && !v.Has("SAMLResponse")
-
-	if showForm {
+	principal := r.Header.Get("Oidc_claim_edupersonprincipalname")
+	if principal == "" { // show mindthegap
 		tmpl.Execute(w, nil)
 		return
 	}
-
-	certs := []string{"MIIC7TCCAdWgAwIBAgIBBzANBgkqhkiG9w0BAQsFADAwMQswCQYDVQQGEwJESzENMAsGA1UEChMEV0FZRjESMBAGA1UEAxMJd2F5Zi4yMDE2MB4XDTE1MDEwMTAwMDAwMFoXDTI1MTIzMTAwMDAwMFowMDELMAkGA1UEBhMCREsxDTALBgNVBAoTBFdBWUYxEjAQBgNVBAMTCXdheWYuMjAxNjCCASIwDQYJKoZIhvcNAQEBBQADggEPADCCAQoCggEBAOeCt61E+O909jreVUV1tFHQe9m3h612W38OauWftVVrwH0CJpYCFGAMUBcDFkPgocA+XpB2qadF+/dnErIDbTVgxqyewB0TOWmMoqMknrkmS0x0AiRHIBtkzIWFam+EwGtFGA5Hw3sjGPoDXg4cGT731uoCktsH5ELt+eFDXSBOUgxyKzZf8NTXRbLksIdPxNgZ04e5JawFo1cnYbTVYQcleMqOYY3rIDXxA8BTm4ZYCNkxLO0v7YK7+mfF5T1Q5C7FXivoQI+A2mi/qGlwLt+oD81jdYki/v7ApXZi0sdcRovA9H4yFCv4tT5f/Plu8YJ8aXSGpJ8gATPtkY9ul9cCAwEAAaMSMBAwDgYDVR0PAQH/BAQDAgIEMA0GCSqGSIb3DQEBCwUAA4IBAQCaDrkKvC9mc8cOmjFhPd/4UgZxol7K0U7GwBY92MXzdE/o4Dd+u3Dw0+O3UsK2kxExFlT3qXuG9XF987xUoGBy+Ip6A14nmKfW9a1hLS7zZoTVpxHebmms8n5voKLPBWowiMwb8jLdVaPzAx7bMnOfXrV3g0L8inPsqgYOgqku9//8I7YnV/r8z0V0uLgi2n9eYDyqvktsL37tIw6RTX/l9J8KQlHy0eWMs9CXDaK1gYdif1EsaHW4xLpjZsohIoovXMtQNTN+jIybXdEDScdLzwT9j9+BU9uHJRx3f3bfwX9QINsDkafDOtBNAnW762LHylOBiXgV2s954JAVY3O+"}
-	saml2jwt := "https://wayf.wayf.dk/saml2jwt"
-	jwtSP := "http://ssh-ca.deic.dk"
-	jwtACS := "https://" + host
-
-	v.Set("acs", jwtACS)
-	v.Set("issuer", jwtSP)
-
-	attrs, err := SAML2jwt(w, saml2jwt, v, certs)
-	if err != nil {
-		log.Panic(err)
-	}
-
-	if attrs == nil {
-		return
-	}
-
-	principal := attrs["eduPersonPrincipalName"].([]interface{})[0].(string)
-	data, err := json.Marshal(attrs)
-
-	token := claims.put(string(data))
 	principal = eppnRegexp.ReplaceAllString(principal, "_")
-
-	err = tmpl.Execute(w, map[string]any{"token": token, "principal": template.JS(principal)})
+	token := claims.put(principal)
+	tmpl.Execute(w, map[string]any{"token": token, "principal": template.JS(principal)})
 	return
 }
 
 func feedbackHandler(w http.ResponseWriter, r *http.Request) {
 	token := strings.TrimPrefix(r.URL.Path, "/feedback/")
 	resp := claims.wait(token)
-	io.WriteString(w, resp)
-}
-
-func SAML2jwt(w http.ResponseWriter, service string, v url.Values, certificates []string) (res map[string]interface{}, err error) {
-	client := &http.Client{
-		Transport: &http.Transport{TLSClientConfig: &tls.Config{InsecureSkipVerify: false}},
-		CheckRedirect: func(req *http.Request, via []*http.Request) error {
-			return http.ErrUseLastResponse
-		},
+	cert := map[string]any{}
+	err := json.Unmarshal([]byte(resp), &cert)
+	if (err != nil) {
+	    io.WriteString(w, resp);
+	    return;
 	}
 
-	body := strings.NewReader(v.Encode())
-	req, _ := http.NewRequest("POST", service, body)
-	req.Header.Add("Content-Type", "application/x-www-form-urlencoded")
-	//	req.Header.Add("Cookie", "wayfid=wayf-qa")
-	resp, err := client.Do(req)
-	if err != nil {
-		return
-	}
-
-	if v["SAMLResponse"] == nil { // ask for a SAMLRequest
-		for header, val := range resp.Header {
-			w.Header().Add(header, val[0]) // Add SAM2jwt headers to our own
-		}
-		w.WriteHeader(302)
-	} else {
-		body, err := ioutil.ReadAll(resp.Body)
-		if err != nil {
-			return nil, err
-		}
-		resp.Body.Close()
-
-		payload, err := jwtVerify(string(body), certificates)
-		if err != nil {
-			return nil, err
-		}
-		var attrs map[string]interface{}
-		err = json.Unmarshal(payload, &attrs)
-		if err != nil {
-			return nil, err
-		}
-		return attrs, nil
-	}
-	return
-}
-
-func jwtVerify(jwt string, certificates []string) (payload []byte, err error) {
-	if len(certificates) == 0 {
-		return payload, errors.New("No Certs found")
-	}
-
-	hps := strings.SplitN(jwt, ".", 3)
-	hp := []byte(strings.Join(hps[:2], "."))
-	headerJSON, _ := base64.RawURLEncoding.DecodeString(hps[0])
-
-	header := struct{ Alg string }{}
-	err = json.Unmarshal(headerJSON, &header)
-	if err != nil {
-		return
-	}
-	var hh crypto.Hash
-	var digest []byte
-	switch header.Alg {
-	case "RS256":
-		dg := sha256.Sum256(hp)
-		digest = dg[:]
-		hh = crypto.SHA256
-	case "RS512":
-		dg := sha512.Sum512(hp)
-		digest = dg[:]
-		hh = crypto.SHA512
-	default:
-		return payload, fmt.Errorf("Unsupported alg: %s", header.Alg)
-	}
-
-	sign, _ := base64.RawURLEncoding.DecodeString(hps[2])
-	var pub *rsa.PublicKey
-	for _, certificate := range certificates {
-		pub, err = cert2publicKey(certificate)
-		if err != nil {
-			return payload, err
-		}
-		err = rsa.VerifyPKCS1v15(pub, hh, digest, sign)
-		if err == nil {
-			payload, err := base64.RawURLEncoding.DecodeString(hps[1])
-			return payload, err
-		}
-	}
-	return
-}
-
-func cert2publicKey(cert string) (publickey *rsa.PublicKey, err error) {
-	key, err := base64.StdEncoding.DecodeString(cert)
-	if err != nil {
-		return
-	}
-	pk, err := x509.ParseCertificate(key)
-	if err != nil {
-		return
-	}
-	publickey = pk.PublicKey.(*rsa.PublicKey)
-	return
+	const iso = "2006-01-02T15:04:05"
+	va := time.Unix(int64(cert["ValidAfter"].(float64)), 0).Format(iso)
+	vb := time.Unix(int64(cert["ValidBefore"].(float64)), 0).Format(iso)
+	io.WriteString(w, fmt.Sprintf("Valid from %s to %s\n\n%s", va, vb, resp))
 }
 
 var clientPubKey ssh.PublicKey
@@ -322,21 +194,16 @@ func sshserver() {
 }
 
 func newCertificate(data string) (cert *ssh.Certificate) {
-	attrs := map[string]any{}
-	err := json.Unmarshal([]byte(data), &attrs)
-	if err != nil {
-		log.Panic(err)
-	}
-	principal := eppnRegexp.ReplaceAllString(attrs["eduPersonPrincipalName"].([]interface{})[0].(string), "_")
+	principal := data
 	cert = &ssh.Certificate{
 		CertType: ssh.UserCert,
 		Key:      clientPubKey,
 		Permissions: ssh.Permissions{
 			CriticalOptions: map[string]string{}, // "force-command": "id ; pwd ; /usr/bin/ls -a"},
-			Extensions:      map[string]string{"permit-agent-forwarding": "", "permit-pty": "", "groups@wayf.dk": string(data)},
+			Extensions:      map[string]string{"permit-agent-forwarding": "", "permit-pty": ""},
 		},
 		KeyId:           principal,
-		ValidPrincipals: []string{principal, "sshfedlogin", "sshweblogin"},
+		ValidPrincipals: []string{principal},
 		ValidAfter:      uint64(time.Now().Unix() - 60),
 		ValidBefore:     uint64(time.Now().Unix() + 36*3600),
 	}
@@ -349,40 +216,6 @@ func newCertificate(data string) (cert *ssh.Certificate) {
 		log.Fatal(err)
 	}
 	return
-}
-
-func (s *bucket) put(v string) (k string) {
-	s.Mutex.Lock()
-	defer s.Mutex.Unlock()
-	k = nonce()
-	s.update()
-	s.Claims[1][k] = v
-	return
-}
-
-func (s *bucket) get(k string) (v string, ok bool) {
-	s.Mutex.RLock()
-	defer s.Mutex.RUnlock()
-	s.update()
-	for i, _ := range "01" {
-		if v, ok = s.Claims[i][k]; ok {
-			delete(s.Claims[i], k) // no-ops if nothing there ...
-			return
-		}
-	}
-	return
-}
-
-func (s *bucket) update() {
-	const ttl = time.Second * 30
-	for _, _ = range "01" {
-		if s.Ttl[0].Before(time.Now()) {
-			s.Claims[0], s.Claims[1] = s.Claims[1], map[string]string{}
-			s.Ttl[0], s.Ttl[1] = s.Ttl[1], time.Now().Add(ttl)
-		} else {
-			return // if 1st bucket is ok no need to check the last
-		}
-	}
 }
 
 func nonce() (s string) {
--- s.sh
+++ s.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 [ ! -z "$1" ] && idplist="?idplist=$1"
-open "https://sshca.lan$idplist"
-nc -l 7778 | ssh -T -p 2022 sshca.lan > ~/.ssh/id_ed25519-cert.pub
+open "https://ssh-cert.dgw.deic.dk/idpentityid=$IDP"; nc -l 7778 | ssh -T -p 2022 service.deic.dk zzz > ~/.ssh/id_deic-sshca-cert.pub-cert.pub
