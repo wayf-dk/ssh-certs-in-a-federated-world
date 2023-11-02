512f300450af2f6c0f44e3d019eb3b5e4dde6131
Mads Freek <freek@wayf.dk>
2023-11-02 11:28:41 +0100

    Removed unnecessary "client" - will be replaced by a shell function

--- s.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-[ ! -z "$1" ] && idplist="?idplist=$1"
-open "https://ssh-cert.dgw.deic.dk/idpentityid=$IDP"; nc -l 7778 | ssh -T -p 2022 service.deic.dk zzz > ~/.ssh/id_deic-sshca-cert.pub-cert.pub
